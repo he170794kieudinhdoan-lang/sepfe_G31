47b01903c1c811977c72e44bb68bbf1db2ac26dd
Doankd <he170794kieudinhdoan@gmail.com>
2026-02-06 09:40:46 +0700

    fix gitignore and project name

--- src/shared/components/Layout/HeaderComp.jsx
+++ src/shared/components/Layout/HeaderComp.jsx
@@ -6,24 +6,24 @@ export const Header = () => {
     return (
         <header className="bg-white sticky top-0 z-50 border-b border-gray-100 shadow-sm">
             <div className="container mx-auto px-4 h-20 flex items-center justify-between">
-                <div className="flex items-center gap-12">
+                <div className="flex items-center gap-3">
                     {/* Logo */}
                     <Link to="/" className="text-3xl font-extrabold tracking-tighter text-primary hover:opacity-90 transition-opacity flex items-center gap-1">
-                        Work<span className="text-gray-800">Link</span>
+                        <img src='/logo_01.png' width={72}></img>
                     </Link>
 
                     {/* Nav */}
                     <nav className="hidden md:flex gap-8 text-sm font-semibold">
-                        <Link to="/" className="text-gray-700 hover:text-primary transition-colors">
+                        <Link to="/" className="text-gray-700 hover:text-yellow-400 transition-colors duration-200">
                             Việc làm
                         </Link>
-                        <Link to="#" className="text-gray-700 hover:text-primary transition-colors">
+                        <Link to="#" className="text-gray-700 hover:text-yellow-400 transition-colors duration-200">
                             Hồ sơ & CV
                         </Link>
-                        <Link to="#" className="text-gray-700 hover:text-primary transition-colors">
+                        <Link to="#" className="text-gray-700 hover:text-yellow-400 transition-colors duration-200">
                             Công ty
                         </Link>
-                        <Link to="/demo/users" className="flex items-center gap-2 text-gray-700 hover:text-primary transition-colors">
+                        <Link to="/demo/users" className="flex items-center gap-2 text-gray-700 hover:text-yellow-200 duration-200 transition-colors">
                             API Demo
                         </Link>
                     </nav>
@@ -31,12 +31,12 @@ export const Header = () => {
 
                 {/* Actions */}
                 <div className="flex items-center gap-3">
-                    <Button variant="ghost" className="text-gray-500 hover:text-primary font-semibold" asChild>
+                    <Button variant="ghost" className="text-gray-500 hover:text-yellow-600 duration-200 transition-colors font-semibold" asChild>
                         <Link to="/auth/login">
                             Đăng nhập
                         </Link>
                     </Button>
-                    <Button className="font-bold px-6 bg-primary hover:bg-primary/90 text-white rounded shadow-md transition-transform active:scale-95">
+                    <Button className="font-bold px-6 bg-yellow-200 hover:text-yellow-700 duration-200 text-black rounded shadow-md transition-transform active:scale-95">
                         Đăng tuyển
                     </Button>
                 </div>
