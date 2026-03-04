44dd1155d7b78e7675146f8d2787253fb8fa1c72
Doankd <he170794kieudinhdoan@gmail.com>
2026-03-04 16:11:41 +0700

    add wishlist section

--- src/shared/api/apiClient.js
+++ src/shared/api/apiClient.js
@@ -87,12 +87,6 @@ export const apiClientCustom = (baseURL) => {
     });
 
     client.interceptors.request.use((config) => {
-        const token = localStorage.getItem('accessToken');
-
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
         console.log(
             'API Request:',
             config.method?.toUpperCase(),
