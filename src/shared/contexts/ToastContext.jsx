36ee6acf259d2d97fb2033db809738faa81665a1
Tuan Sang <tuansangsb2k3@gmail.com>
2026-03-06 10:08:46 +0700

    FIX: fix color background of toast

--- src/shared/contexts/ToastContext.jsx
+++ src/shared/contexts/ToastContext.jsx
@@ -31,50 +31,16 @@ export const ToastProvider = ({ children }) => {
 };
 
 const ToastList = ({ toasts }) => (
-  <div className="fixed top-18 right-5 z-[100] flex flex-col gap-3">
+  <div className="fixed top-20 right-5 z-100 flex flex-col gap-3">
     {toasts.map((t) => (
       <div
         key={t.id}
-        className={`flex items-start gap-3 rounded-2xl px-4 py-3 shadow-xl min-w-[300px] max-w-sm text-sm font-medium border transition-all duration-300 animate-slideIn ${
+        className={`flex items-start gap-3 rounded-2xl px-4 py-3 shadow-xl min-w-[300px] max-w-sm text-sm font-medium border backdrop-blur-md transition-all duration-300 animate-slideIn ${
           t.type === 'error'
-            ? 'bg-red-600 border-red-700 text-white shadow-red-500/40'
-            : 'bg-emerald-600 border-emerald-700 text-white shadow-emerald-500/40'
+            ? 'bg-red-500/10 border-red-500/30 text-red-600'
+            : 'bg-emerald-500/10 border-emerald-500/30 text-emerald-600'
         }`}
       >
-        {/* <div className="flex-1 flex items-center gap-2">
-          {t.type === 'error' ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 shrink-0"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 shrink-0"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          )}
-          {t.message}
-        </div> */}
         <div className="flex-1">{t.message}</div>
       </div>
     ))}
