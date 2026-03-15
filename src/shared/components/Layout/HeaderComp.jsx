eb5698534498c21c639d6aec271b2f14a67e9a84
Tuan Sang <tuansangsb2k3@gmail.com>
2026-03-15 18:28:47 +0700

    FIX: fix icon message not display

--- src/shared/components/Layout/HeaderComp.jsx
+++ src/shared/components/Layout/HeaderComp.jsx
@@ -317,7 +317,7 @@ export const Header = () => {
               <Link to="/chat">
                 <MessageCircle className="h-5 w-5" />
                 {unreadCountChat > 0 && (
-                  <span className="absolute  min-w-5 h-5 px-1 rounded-full bg-red-500 text-white text-[10px] leading-5 font-semibold text-center ring-2 ring-white">
+                  <span className="absolute -right-1 -top-1 min-w-5 h-5 px-1 rounded-full bg-red-500 text-white text-[10px] leading-5 font-semibold text-center ring-2 ring-white">
                     {unreadCountChat > 99 ? '99+' : unreadCountChat}
                   </span>
                 )}
